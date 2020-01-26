8f67fd3a61d68090f0aba986d7e476cb8c8fb92e
unknown <IT Department@AIMSNEI-LTP-088.ad.nexteinstein.org>
2020-01-27 00:19:54 +0200

    Counter

--- vueJS/js/app.js
+++ vueJS/js/app.js
@@ -3,7 +3,9 @@
 new Vue({
     el:'#app',
     data:{
-        title:'Hello Moses'
+        title:'Hello Moses',
+        link:'https://google.com',
+        finishedLink:'<a href="https://google.com">Google</a>'
     },
     methods:{
         sayHello:function(){
@@ -12,3 +14,16 @@ new Vue({
     }
 
 }); 
+
+new Vue({
+  el :".mose",
+  data: {
+      counter: 0
+  },
+  methods:{
+      increase: function(){
+          this.counter++;
+      }
+  }
+
+});
