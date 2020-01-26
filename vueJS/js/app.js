2c8ab79de75f49a019e6ac02d4aa155671e5dae9
unknown <IT Department@AIMSNEI-LTP-088.ad.nexteinstein.org>
2020-01-26 19:16:05 +0200

    Using a method in vuejs

--- vueJS/js/app.js
+++ vueJS/js/app.js
@@ -4,5 +4,11 @@ new Vue({
     el:'#app',
     data:{
         title:'Hello Moses'
+    },
+    methods:{
+        sayHello:function(){
+            return this.title;
+        }
     }
-});
+
+}); 
